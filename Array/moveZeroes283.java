4cf61375a447503b149f375d62315a59e8a81c43
Mayur Tekale <mayurtekale7@gmail.com>
2024-09-21 15:19:02 +0530

    Array Two pointer

---
+++ Array/moveZeroes283.java
@@ -0,0 +1,32 @@
+//https://leetcode.com/problems/move-zeroes/description/?envType=problem-list-v2&envId=array&difficulty=EASY
+package Array;
+
+import java.util.Arrays;
+
+/**
+ * moveZeroes283
+ */
+public class moveZeroes283 {
+    public static void main(String[] args) {
+        int arr[] ={0,1,0,3,1,0,52};
+       
+       System.out.println(Arrays.toString(moveZeroes(arr)));
+          
+        
+    }
+   //change int[] method to void for leetcode
+    public static int[] moveZeroes(int[] arr) {
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i] != 0){
+                arr[count++] = arr[i];
+            }
+            
+        }
+        while (count < arr.length) {
+            arr[count++] = 0;
+        }
+        return arr;
+           
+    }
+}

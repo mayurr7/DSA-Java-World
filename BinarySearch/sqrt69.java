53b7b1a9f92660e331e1c9505dd70101fd63e4fb
Mayur Tekale <mayurtekale7@gmail.com>
2024-09-26 10:06:53 +0530

    Binary Search

---
+++ BinarySearch/sqrt69.java
@@ -0,0 +1,21 @@
+package BinarySearch;
+
+public class sqrt69 {
+    public static void main(String[] args) {
+        System.out.println(mySqrt(26));
+    }
+    public static int mySqrt(int x) {
+        if(x == 0) return 0;
+        int start = 1; int end = x; int ans= 1;
+        while (start <= end) {
+            int mid = start+ (end - start) /2;
+            if(mid  >= x / mid){
+                end = mid - 1; 
+                ans = mid;
+            }else{
+                start = mid + 1;
+               
+            }
+        }return ans;
+    }
+}
---
+++ BinarySearch/validPerfectSqr367.java
@@ -0,0 +1,31 @@
+package BinarySearch;
+
+/**
+ * validPerfectSqr367
+ */
+public class validPerfectSqr367 {
+    public static void main(String[] args) {
+        System.out.println(isPerfectSquare(17));
+    }
+    public static boolean isPerfectSquare(int num) {
+        int ans = mySqrt(num);
+        if(ans *ans == num) {
+            return true;
+        }return false;
+    }
+    public static int mySqrt(int x) {
+        if(x == 0) return 0;
+        int start = 1; int end = x; int ans= 1;
+        while (start <= end) {
+            int mid = start+ (end - start) /2;
+            if(mid  >= x / mid){
+                end = mid - 1; 
+                ans = mid;
+            }else{
+                start = mid + 1;
+               
+            }
+        }return ans;
+    }
+    
+}

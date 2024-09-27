4fe3fe1dd505e5907fb486702b773bae285e6705
Mayur Tekale <mayurtekale7@gmail.com>
2024-09-27 11:10:16 +0530

    bubble sort

---
+++ Sorting/bubbleSort.java
@@ -0,0 +1,28 @@
+package Sorting;
+
+import java.util.Arrays;
+
+public class bubbleSort {
+    public static void main(String[] args) {
+        int arr[] = {1,2,3,4};
+        bubble(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    static void bubble(int arr[]){
+        boolean isSorted = false;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 1; j < arr.length-i; j++) {
+                if(arr[j] < arr[j-1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j-1];
+                    arr[j-1] = temp;
+                    isSorted = true;
+                }
+            }
+            if(isSorted = true){
+                break;
+            }
+        }
+        
+    }
+}

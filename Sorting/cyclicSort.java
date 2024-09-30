3550cacf22cdaf2adc61944bc4a82cef22310038
Mayur Tekale <mayurtekale7@gmail.com>
2024-09-30 11:24:40 +0530

    cyclic sort

---
+++ Sorting/cyclicSort.java
@@ -0,0 +1,30 @@
+package Sorting;
+
+import java.util.Arrays;
+
+public class cyclicSort {
+    public static void main(String[] args) {
+        int arr[] = {1,5,2,3,4};
+        cyclicsort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    static void cyclicsort(int arr[]){
+       int i =0;
+       while (i < arr.length) {
+        int correctIndex = arr[i] - 1;
+            if(arr[i] != arr[correctIndex]){
+                    swap(arr, i, correctIndex);
+            }else{
+                i++;
+                }
+       }
+    }
+
+    
+    static void swap(int arr[], int i , int j){
+            int temp = arr[i];
+            arr[i] =arr[j];
+            arr[j] = temp;
+    }
+         
+}
---
+++ Sorting/findMissing268.java
@@ -0,0 +1,40 @@
+//https://leetcode.com/problems/missing-number/
+package Sorting;
+
+public class findMissing268 {
+    public static void main(String[] args) {
+        int arr[] ={0,1};
+        System.out.println(missingNumber(arr));
+    }
+
+ 
+        public static int missingNumber(int[] arr) {
+        sort(arr);
+        int n = arr.length;
+           for(int i =0; i< arr.length; i++){
+                if(arr[i] != i){
+                    return i;
+                }
+            }
+           return n;
+        }
+    
+        public static void sort(int arr[]){
+             int i =0; 
+            while(i < arr.length){
+                int correctIndex = arr[i];
+                if(arr[i] < arr.length && arr[i] != arr[correctIndex]){
+                    swapp(arr, i, correctIndex);
+                }else{
+                    i++;
+                }
+            }
+        }
+    
+        public static void swapp(int arr[] , int i, int j){
+            int temp = arr[i];
+                arr[i] =arr[j];
+                arr[j] = temp;
+        }
+    
+}

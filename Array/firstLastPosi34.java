11862ef3e7ab7399ef11d0db9504a1111f8473d2
Mayur Tekale <mayurtekale7@gmail.com>
2024-11-03 17:48:04 +0530

    arr

--- Array/firstLastPosi34.java
+++ Array/firstLastPosi34.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class firstLastPosi34 {
     public static void main(String[] args) {
-        int arr[] ={1,2,3,3,3};
+        int arr[] = { 1, 2, 3, 3, 3 };
         System.out.println(Arrays.toString(searchRange(arr, 3)));
     }
 
@@ -12,45 +12,48 @@ public class firstLastPosi34 {
         int first = searchFirstRange(nums, target);
         int last = searchLastRange(nums, target);
 
-        return new int[]{first,last};
+        return new int[] { first, last };
     }
 
     public static int searchFirstRange(int[] arr, int target) {
-       int first =-1; int start =0; int end =arr.length-1;
+        int first = -1;
+        int start = 0;
+        int end = arr.length - 1;
 
-       while (start <= end) {
-            int mid = start + (end - start)/2;
-            if(arr[mid] == target){
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (arr[mid] == target) {
                 first = mid;
-                end = mid -1;
-            }else if(arr[mid] < target){
-                start = mid +1;
-            }else{
-                end = mid -1;
+                end = mid - 1;
+            } else if (arr[mid] < target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
             }
-           
-       }
-       return first;
-       
-    } 
-    
+
+        }
+        return first;
+
+    }
 
     public static int searchLastRange(int[] arr, int target) {
-        int last =-1; int start =0; int end =arr.length-1;
- 
+        int last = -1;
+        int start = 0;
+        int end = arr.length - 1;
+
         while (start <= end) {
-             int mid = start + (end - start)/2;
-             if(arr[mid] == target){
-                 last = mid;
-                 start = mid +1;
-             }else if(arr[mid] < target){
-                 start = mid +1;
-             }else{
-                 end = mid -1;
-             }
-            
+            int mid = start + (end - start) / 2;
+            if (arr[mid] == target) {
+                last = mid;
+                start = mid + 1;
+            } else if (arr[mid] < target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+
         }
         return last;
-        
-     } 
+
+    }
 }

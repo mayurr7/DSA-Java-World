684bd8b0dae3b5491aa7fa0f91255584cf22c3b7
Mayur Tekale <mayurtekale7@gmail.com>
2024-11-07 21:25:34 +0530

    math

---
+++ Math/prime.java
@@ -0,0 +1,24 @@
+package Math;
+
+public class prime {
+    public static void main(String[] args) {
+        int n=40;
+        boolean arr[] = new boolean[n+1];
+       isPrime(n, arr);
+    }
+    static void isPrime(int n, boolean arr[]){
+        for (int i = 2; i * i <= n; i++) {
+            if(!arr[i]){
+                for (int j = i*2; j <= n; j+=i) {
+                    arr[j] = true;
+                }
+            }
+        }
+
+        for (int i = 2; i <= n; i++) {
+            if(!arr[i]){
+                System.out.println(i + " ");
+            }
+        }
+    }
+}

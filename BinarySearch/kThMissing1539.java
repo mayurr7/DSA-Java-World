9cf1c8f513ac731227f43472472b4ac89ee08856
Mayur Tekale <mayurtekale7@gmail.com>
2025-01-29 11:51:10 +0530

    update

--- BinarySearch/kThMissing1539.java
+++ BinarySearch/kThMissing1539.java
@@ -2,12 +2,12 @@ package BinarySearch;
 
 public class kThMissing1539 {
     public static void main(String[] args) {
-        int arr[] ={4,7,8};
-        System.out.println(findKthPositive(arr, 3));
+        int arr[] ={2,3,4,7,11};
+        System.out.println(findKthPositive(arr, 5));
     }
     public static int findKthPositive(int[] arr, int k) {
        int start = 0; int end = arr.length-1;
-       while (start <= end) {
+       while (start < end) {
             int mid = start + (end - start)/2;
             int tottleMissing = arr[mid] - (mid + 1);
 
@@ -17,6 +17,6 @@ public class kThMissing1539 {
                 end = mid - 1;
             }
        }
-       return end + k +1;
+       return start + k;
     }
 }

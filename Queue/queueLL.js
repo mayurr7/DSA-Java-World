eca8b3cfbf82ea4980774be8a469df23946f4f55
Mayur Tekale <mayurtekale7@gmail.com>
2025-03-15 12:03:31 +0530

    update

--- Queue/queueLL.js
+++ Queue/queueLL.js
@@ -1,20 +1,90 @@
 class Node{
     constructor(val){
-        this.val = this.val;
+        this.val = val;
         this.next = null;
     }
 }
 
 class Queue{
     constructor(){
-        this.top = null;
+        this.front = null;
+        this.rear = null;
 
     }
 
     enqueue(x){
         let node =  new Node(x);
-        this.node.next = this.top;
+        
+        if(this.rear == null){
+            this.front = this.rear = node;
+            return;
+        }
+
+        this.rear.next = node;
+        this.rear = node;
+        
+    }
+
+    dequeue(){
+        if(this.front === null) return -1;
+
+        let popVal = this.front.val;
+        this.front = this.front.next;
+
+        if(this.front === null){
+            this.rear = null;
+        }
+
+        return popVal;
+    }
+
+    peek(){
+        if(this.front === null) return -1;
+
+        return this.front.val;
+    }
+
+    isEmpty(){
+        return this.front === null;
+    }
+
+    print(){
+        let arr = [];
+        let curr = this.front;
+
+        while(curr !== null){
+            arr.push(curr.val);
+            curr = curr.next;
+        }
+
+        console.log(arr.join("->"));
         
     }
 
 }
+
+let queue = new Queue();
+
+queue.enqueue(10);
+queue.enqueue(11);
+queue.enqueue(12);
+queue.enqueue(13);
+queue.enqueue(104);
+
+
+queue.print();
+console.log(queue.dequeue());
+console.log(queue.peek());
+queue.dequeue();
+queue.dequeue();
+queue.dequeue();
+queue.dequeue();
+queue.dequeue();
+console.log(queue.peek());
+queue.enqueue(1000);
+queue.print();
+
+console.log(queue.isEmpty());
+
+
+
--- Recursion/fiboonachiNum.java
+++ Recursion/fiboonachiNum.java
@@ -2,15 +2,15 @@ package Recursion;
 
 public class fiboonachiNum {
     public static void main(String[] args) {
-        System.out.println(fibbo(5));
+        System.out.println(fibbo(10));
     }
 
     static int fibbo(int n){
-        // if(n < 2) return n;
+        if(n < 2) return n;
 
-        // return fibbo(n - 1) + fibbo(n - 2);
+        return fibbo(n - 1) + fibbo(n - 2);
 
-        return (int) Math.round(Math.pow((1+ Math.sqrt(5)) / 2, n)/Math.sqrt(5));
+        //return (int) Math.round(Math.pow((1+ Math.sqrt(5)) / 2, n)/Math.sqrt(5));
 
     }
 
